573dc8772ec66b7748b242db2891d0eed2a4d66a
Emam Mounqer <emam.anagreh@gmail.com>
2023-04-17 09:59:43 +0000

    add game_logic class without implementation

---
+++ ten_thousand/game_logic.py
@@ -0,0 +1,14 @@
+
+
+class GameLogic:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def calculate_score(cls, dice_values):
+        pass
+
+    @classmethod
+    def roll_dice(cls, num_of_dices):
+        pass
